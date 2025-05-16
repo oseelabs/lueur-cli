4c7efb8c7716ca742117eb923dd840e0ae6231aa
Lazaro Osee <odiraoseelazaro@students.tukenya.ac.ke>
2025-05-17 00:49:41 +0300

    Initial commit

---
+++ bin/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import { Command } from "commander";
+import figlet from "figlet";
+figlet("Lueur  CLI", {
+    width: 80,
+}, (err, data) => {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(chalk.hex("#ff6600")(data));
+});
+async function run() {
+    const program = new Command('lueur');
+    program
+        .option('-t, --text [text]', 'Test console log', 'Hello, Guest')
+        .parse();
+    const [] = program.args;
+    const options = program.opts();
+    console.log(chalk.green(`${options.text}`));
+}
+;
+await run();
---
+++ setup.sh
@@ -0,0 +1,6 @@
+npm i commander figlet chalk
+npm i -D @types/node typescript
+npx tsc --init
+npm i -D eslint
+npm i -D @typescript-eslint/parser @typescript-eslint/eslint-plugin
+npm i -D --exact prettier
